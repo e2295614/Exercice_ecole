9a6b6c2b07df983fcab6ae51aad91ace918429ea
e2295614 <100293429+e2295614@users.noreply.github.com>
2022-03-14 10:00:00 -0400

    Create Exercice ordre croissant.py

---
+++ Exercice ordre croissant.py
@@ -0,0 +1,12 @@
+#Créer une fonction prenant 4 paramètres et les retournant en ordre croissant en utilisant que des conditions(en n’utilisant pas de loops).
+
+def ordre_croissant(num1,num2,num3,num4):
+
+ num1 = 1
+ num2 = 2
+ num3 = 3
+ num4 = 4
+
+if(1<=2<=3<4):
+    print("1,2,3,4")
+
